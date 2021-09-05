3fed2db8753f41f9d76bc08709f0024f7ba0a7a1
Jace <638764+jacebenson@users.noreply.github.com>
2021-09-05 06:39:43 -0500

    added usedby section

---
+++ src/_data/usedby.js
@@ -0,0 +1,18 @@
+module.exports = [
+    
+    {
+        name: "TeamStream.gg",
+        image: "/images/teamstreamgg.jpg",
+        url: "https://teamstream.gg"
+    },
+    {
+        name: "Everfun",
+        image: "/images/everfun.png",
+        url: "https://everfun.dev"
+    },
+    {
+        name: "Snaplet",
+        image: "/images/snaplet.svg",
+        url: "https://snaplet.dev"
+    }
+]
